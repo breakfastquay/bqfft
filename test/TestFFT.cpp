05ea449a67095e5fe98bd9223a7aecac0440f97b
Chris Cannam <cannam@all-day-breakfast.com>
2019-04-03 11:44:57 +0100

    Test all (compiled-in) implementations

--- test/TestFFT.cpp
+++ test/TestFFT.cpp
@@ -49,13 +49,19 @@ using namespace breakfastquay;
 
 BOOST_AUTO_TEST_SUITE(TestFFT)
 
-#ifdef TEST_FFT_IS_SINGLE_PRECISION_ONLY
-static const double eps = 1e-7;
-static const float epsf = 1e-6f;
-#else
-static const double eps = 1e-14;
-static const float epsf = 1e-6f;
-#endif
+#define DEFINE_EPS(fft) \
+    float epsf = 1e-6f; \
+    double eps; \
+    if (fft.getSupportedPrecisions() & FFT::DoublePrecision) { \
+        eps = 1e-14; \
+    } else { \
+        eps = 1e-7; \
+    } \
+    (void)epsf; (void)eps;
+
+#define USING_FFT(n) \
+    FFT fft(n); \
+    DEFINE_EPS(fft);
 
 #define COMPARE(a, b) BOOST_CHECK_SMALL(a-b, eps) 
 #define COMPARE_F(a, b) BOOST_CHECK_SMALL(a-b, epsf) 
@@ -81,65 +87,111 @@ static const float epsf = 1e-6f;
         BOOST_CHECK_SMALL(a[cmp_i]/s - b[cmp_i], epsf); \
     }
 
-BOOST_AUTO_TEST_CASE(dc)
+#define ONE_IMPL_AUTO_TEST_CASE(name, impl) \
+    BOOST_AUTO_TEST_CASE(name##_##impl) \
+    { \
+        std::set<std::string> impls = FFT::getImplementations(); \
+        if (impls.find(#impl) == impls.end()) return; \
+        FFT::setDefaultImplementation(#impl); \
+        performTest_##name(); \
+        FFT::setDefaultImplementation(""); \
+    }
+
+// If you add an implementation in FFT.cpp, add it also to
+// ALL_IMPL_AUTO_TEST_CASE and all_implementations[] below
+
+#define ALL_IMPL_AUTO_TEST_CASE(name) \
+    void performTest_##name (); \
+    ONE_IMPL_AUTO_TEST_CASE(name, ipp); \
+    ONE_IMPL_AUTO_TEST_CASE(name, fftw); \
+    ONE_IMPL_AUTO_TEST_CASE(name, kissfft); \
+    ONE_IMPL_AUTO_TEST_CASE(name, vdsp); \
+    ONE_IMPL_AUTO_TEST_CASE(name, medialib); \
+    ONE_IMPL_AUTO_TEST_CASE(name, openmax); \
+    ONE_IMPL_AUTO_TEST_CASE(name, sfft); \
+    ONE_IMPL_AUTO_TEST_CASE(name, cross); \
+    ONE_IMPL_AUTO_TEST_CASE(name, dft); \
+    void performTest_##name ()
+
+std::string all_implementations[] = {
+    "ipp", "vdsp", "fftw", "sfft", "openmax",
+    "medialib", "kissfft", "cross", "dft"
+};
+
+BOOST_AUTO_TEST_CASE(showImplementations)
+{
+    std::set<std::string> impls = FFT::getImplementations();
+    std::cerr << "\nThe following implementations are compiled in and will be tested:" << std::endl;
+    for (int i = 0; i < int(sizeof(all_implementations)/sizeof(all_implementations[0])); ++i) {
+        if (impls.find(all_implementations[i]) != impls.end()) {
+            std::cerr << " +" << all_implementations[i];
+        }
+    }
+    std::cerr << std::endl << std::endl;
+    std::cerr << "The following implementations are NOT compiled in and will not be tested:" << std::endl;
+    for (int i = 0; i < int(sizeof(all_implementations)/sizeof(all_implementations[0])); ++i) {
+        if (impls.find(all_implementations[i]) == impls.end()) {
+            std::cerr << " -" << all_implementations[i];
+        }
+    }
+    std::cerr << std::endl << std::endl;
+}
+
+ALL_IMPL_AUTO_TEST_CASE(dc)
 {
     // DC-only signal. The DC bin is purely real
     double in[] = { 1, 1, 1, 1 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE(re[0], 4.0);
     COMPARE_ZERO(re[1]);
     COMPARE_ZERO(re[2]);
     COMPARE_ALL(im, 0.0);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(showImplementation)
-{
-    FFT f(4); // to ensure an implementation has been picked
-    (void)f;  // avoid compiler warning
-    std::cerr << "Using implementation: " << FFT::getDefaultImplementation()
-              << std::endl;
-}
-
-BOOST_AUTO_TEST_CASE(sine)
+ALL_IMPL_AUTO_TEST_CASE(sine)
 {
     // Sine. Output is purely imaginary
     double in[] = { 0, 1, 0, -1 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ALL(re, 0.0);
     COMPARE_ZERO(im[0]);
     COMPARE(im[1], -2.0);
     COMPARE_ZERO(im[2]);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cosine)
+ALL_IMPL_AUTO_TEST_CASE(cosine)
 {
     // Cosine. Output is purely real
     double in[] = { 1, 0, -1, 0 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO(re[0]);
     COMPARE(re[1], 2.0);
     COMPARE_ZERO(re[2]);
     COMPARE_ALL(im, 0.0);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 	
-BOOST_AUTO_TEST_CASE(sineCosine)
+ALL_IMPL_AUTO_TEST_CASE(sineCosine)
 {
     // Sine and cosine mixed
     double in[] = { 0.5, 1, -0.5, -1 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO(re[0]);
     COMPARE(re[1], 1.0);
     COMPARE_ZERO(re[2]);
@@ -147,30 +199,32 @@ BOOST_AUTO_TEST_CASE(sineCosine)
     COMPARE(im[1], -2.0);
     COMPARE_ZERO(im[2]);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(nyquist)
+ALL_IMPL_AUTO_TEST_CASE(nyquist)
 {
     double in[] = { 1, -1, 1, -1 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO(re[0]);
     COMPARE_ZERO(re[1]);
     COMPARE(re[2], 4.0);
     COMPARE_ALL(im, 0.0);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 	
-BOOST_AUTO_TEST_CASE(interleaved)
+ALL_IMPL_AUTO_TEST_CASE(interleaved)
 {
     // Sine and cosine mixed, test output format
     double in[] = { 0.5, 1, -0.5, -1 };
     double out[6];
-    FFT(4).forwardInterleaved(in, out);
+    USING_FFT(4);
+    fft.forwardInterleaved(in, out);
     COMPARE_ZERO(out[0]);
     COMPARE_ZERO(out[1]);
     COMPARE(out[2], 1.0);
@@ -178,15 +232,16 @@ BOOST_AUTO_TEST_CASE(interleaved)
     COMPARE_ZERO(out[4]);
     COMPARE_ZERO(out[5]);
     double back[4];
-    FFT(4).inverseInterleaved(out, back);
+    fft.inverseInterleaved(out, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cosinePolar)
+ALL_IMPL_AUTO_TEST_CASE(cosinePolar)
 {
     double in[] = { 1, 0, -1, 0 };
     double mag[3], phase[3];
-    FFT(4).forwardPolar(in, mag, phase);
+    USING_FFT(4);
+    fft.forwardPolar(in, mag, phase);
     COMPARE_ZERO(mag[0]);
     COMPARE(mag[1], 2.0);
     COMPARE_ZERO(mag[2]);
@@ -194,15 +249,16 @@ BOOST_AUTO_TEST_CASE(cosinePolar)
     // could legitimately be anything)
     COMPARE_ZERO(phase[1]);
     double back[4];
-    FFT(4).inversePolar(mag, phase, back);
+    fft.inversePolar(mag, phase, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(sinePolar)
+ALL_IMPL_AUTO_TEST_CASE(sinePolar)
 {
     double in[] = { 0, 1, 0, -1 };
     double mag[3], phase[3];
-    FFT(4).forwardPolar(in, mag, phase);
+    USING_FFT(4);
+    fft.forwardPolar(in, mag, phase);
     COMPARE_ZERO(mag[0]);
     COMPARE(mag[1], 2.0);
     COMPARE_ZERO(mag[2]);
@@ -210,42 +266,45 @@ BOOST_AUTO_TEST_CASE(sinePolar)
     // could legitimately be anything)
     COMPARE(phase[1], -M_PI/2.0);
     double back[4];
-    FFT(4).inversePolar(mag, phase, back);
+    fft.inversePolar(mag, phase, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(magnitude)
+ALL_IMPL_AUTO_TEST_CASE(magnitude)
 {
     // Sine and cosine mixed
     double in[] = { 0.5, 1, -0.5, -1 };
     double out[3];
-    FFT(4).forwardMagnitude(in, out);
+    USING_FFT(4);
+    fft.forwardMagnitude(in, out);
     COMPARE_ZERO(out[0]);
     COMPARE_F(float(out[1]), sqrtf(5.0));
     COMPARE_ZERO(out[2]);
 }
 
-BOOST_AUTO_TEST_CASE(dirac)
+ALL_IMPL_AUTO_TEST_CASE(dirac)
 {
     double in[] = { 1, 0, 0, 0 };
     double re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE(re[0], 1.0);
     COMPARE(re[1], 1.0);
     COMPARE(re[2], 1.0);
     COMPARE_ALL(im, 0.0);
     double back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cepstrum)
+ALL_IMPL_AUTO_TEST_CASE(cepstrum)
 {
     double in[] = { 1, 0, 0, 0, 1, 0, 0, 0 };
     double mag[5];
-    FFT(8).forwardMagnitude(in, mag);
+    USING_FFT(8);
+    fft.forwardMagnitude(in, mag);
     double cep[8];
-    FFT(8).inverseCepstral(mag, cep);
+    fft.inverseCepstral(mag, cep);
     BOOST_CHECK_SMALL(cep[1], 1e-9);
     BOOST_CHECK_SMALL(cep[2], 1e-9);
     BOOST_CHECK_SMALL(cep[3], 1e-9);
@@ -256,14 +315,15 @@ BOOST_AUTO_TEST_CASE(cepstrum)
     BOOST_CHECK_SMALL( 7.254329 - cep[4]/8, 0.000001);
 }
 
-BOOST_AUTO_TEST_CASE(forwardArrayBounds)
+ALL_IMPL_AUTO_TEST_CASE(forwardArrayBounds)
 {
     // initialise bins to something recognisable, so we can tell
     // if they haven't been written
     double in[] = { 1, 1, -1, -1 };
     double re[] = { 999, 999, 999, 999, 999 };
     double im[] = { 999, 999, 999, 999, 999 };
-    FFT(4).forward(in, re+1, im+1);
+    USING_FFT(4);
+    fft.forward(in, re+1, im+1);
     // And check we haven't overrun the arrays
     COMPARE(re[0], 999.0);
     COMPARE(im[0], 999.0);
@@ -271,70 +331,75 @@ BOOST_AUTO_TEST_CASE(forwardArrayBounds)
     COMPARE(im[4], 999.0);
 }
 
-BOOST_AUTO_TEST_CASE(inverseArrayBounds)
+ALL_IMPL_AUTO_TEST_CASE(inverseArrayBounds)
 {
     // initialise bins to something recognisable, so we can tell
     // if they haven't been written
     double re[] = { 0, 1, 0 };
     double im[] = { 0, -2, 0 };
     double out[] = { 999, 999, 999, 999, 999, 999 };
-    FFT(4).inverse(re, im, out+1);
+    USING_FFT(4);
+    fft.inverse(re, im, out+1);
     // And check we haven't overrun the arrays
     COMPARE(out[0], 999.0);
     COMPARE(out[5], 999.0);
 }
 
-BOOST_AUTO_TEST_CASE(dcF)
+ALL_IMPL_AUTO_TEST_CASE(dcF)
 {
     // DC-only signal. The DC bin is purely real
     float in[] = { 1, 1, 1, 1 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_F(re[0], 4.0f);
     COMPARE_ZERO_F(re[1]);
     COMPARE_ZERO_F(re[2]);
     COMPARE_ALL_F(im, 0.0f);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(sineF)
+ALL_IMPL_AUTO_TEST_CASE(sineF)
 {
     // Sine. Output is purely imaginary
     float in[] = { 0, 1, 0, -1 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ALL_F(re, 0.0f);
     COMPARE_ZERO_F(im[0]);
     COMPARE_F(im[1], -2.0f);
     COMPARE_ZERO_F(im[2]);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cosineF)
+ALL_IMPL_AUTO_TEST_CASE(cosineF)
 {
     // Cosine. Output is purely real
     float in[] = { 1, 0, -1, 0 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO_F(re[0]);
     COMPARE_F(re[1], 2.0f);
     COMPARE_ZERO_F(re[2]);
     COMPARE_ALL_F(im, 0.0f);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 	
-BOOST_AUTO_TEST_CASE(sineCosineF)
+ALL_IMPL_AUTO_TEST_CASE(sineCosineF)
 {
     // Sine and cosine mixed
     float in[] = { 0.5, 1, -0.5, -1 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO_F(re[0]);
     COMPARE_F(re[1], 1.0f);
     COMPARE_ZERO_F(re[2]);
@@ -342,30 +407,32 @@ BOOST_AUTO_TEST_CASE(sineCosineF)
     COMPARE_F(im[1], -2.0f);
     COMPARE_ZERO_F(im[2]);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(nyquistF)
+ALL_IMPL_AUTO_TEST_CASE(nyquistF)
 {
     float in[] = { 1, -1, 1, -1 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_ZERO_F(re[0]);
     COMPARE_ZERO_F(re[1]);
     COMPARE_F(re[2], 4.0f);
     COMPARE_ALL_F(im, 0.0f);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 	
-BOOST_AUTO_TEST_CASE(interleavedF)
+ALL_IMPL_AUTO_TEST_CASE(interleavedF)
 {
     // Sine and cosine mixed, test output format
     float in[] = { 0.5, 1, -0.5, -1 };
     float out[6];
-    FFT(4).forwardInterleaved(in, out);
+    USING_FFT(4);
+    fft.forwardInterleaved(in, out);
     COMPARE_ZERO_F(out[0]);
     COMPARE_ZERO_F(out[1]);
     COMPARE_F(out[2], 1.0f);
@@ -373,15 +440,16 @@ BOOST_AUTO_TEST_CASE(interleavedF)
     COMPARE_ZERO_F(out[4]);
     COMPARE_ZERO_F(out[5]);
     float back[4];
-    FFT(4).inverseInterleaved(out, back);
+    fft.inverseInterleaved(out, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cosinePolarF)
+ALL_IMPL_AUTO_TEST_CASE(cosinePolarF)
 {
     float in[] = { 1, 0, -1, 0 };
     float mag[3], phase[3];
-    FFT(4).forwardPolar(in, mag, phase);
+    USING_FFT(4);
+    fft.forwardPolar(in, mag, phase);
     COMPARE_ZERO_F(mag[0]);
     COMPARE_F(mag[1], 2.0f);
     COMPARE_ZERO_F(mag[2]);
@@ -389,15 +457,16 @@ BOOST_AUTO_TEST_CASE(cosinePolarF)
     // could legitimately be anything)
     COMPARE_ZERO_F(phase[1]);
     float back[4];
-    FFT(4).inversePolar(mag, phase, back);
+    fft.inversePolar(mag, phase, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(sinePolarF)
+ALL_IMPL_AUTO_TEST_CASE(sinePolarF)
 {
     float in[] = { 0, 1, 0, -1 };
     float mag[3], phase[3];
-    FFT(4).forwardPolar(in, mag, phase);
+    USING_FFT(4);
+    fft.forwardPolar(in, mag, phase);
     COMPARE_ZERO_F(mag[0]);
     COMPARE_F(mag[1], 2.0f);
     COMPARE_ZERO_F(mag[2]);
@@ -405,42 +474,45 @@ BOOST_AUTO_TEST_CASE(sinePolarF)
     // could legitimately be anything)
     COMPARE_F(phase[1], -float(M_PI)/2.0f);
     float back[4];
-    FFT(4).inversePolar(mag, phase, back);
+    fft.inversePolar(mag, phase, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(magnitudeF)
+ALL_IMPL_AUTO_TEST_CASE(magnitudeF)
 {
     // Sine and cosine mixed
     float in[] = { 0.5, 1, -0.5, -1 };
     float out[3];
-    FFT(4).forwardMagnitude(in, out);
+    USING_FFT(4);
+    fft.forwardMagnitude(in, out);
     COMPARE_ZERO_F(out[0]);
     COMPARE_F(float(out[1]), sqrtf(5.0f));
     COMPARE_ZERO_F(out[2]);
 }
 
-BOOST_AUTO_TEST_CASE(diracF)
+ALL_IMPL_AUTO_TEST_CASE(diracF)
 {
     float in[] = { 1, 0, 0, 0 };
     float re[3], im[3];
-    FFT(4).forward(in, re, im);
+    USING_FFT(4);
+    fft.forward(in, re, im);
     COMPARE_F(re[0], 1.0f);
     COMPARE_F(re[1], 1.0f);
     COMPARE_F(re[2], 1.0f);
     COMPARE_ALL_F(im, 0.0f);
     float back[4];
-    FFT(4).inverse(re, im, back);
+    fft.inverse(re, im, back);
     COMPARE_SCALED_F(back, in, 4);
 }
 
-BOOST_AUTO_TEST_CASE(cepstrumF)
+ALL_IMPL_AUTO_TEST_CASE(cepstrumF)
 {
     float in[] = { 1, 0, 0, 0, 1, 0, 0, 0 };
     float mag[5];
-    FFT(8).forwardMagnitude(in, mag);
+    USING_FFT(8);
+    fft.forwardMagnitude(in, mag);
     float cep[8];
-    FFT(8).inverseCepstral(mag, cep);
+    fft.inverseCepstral(mag, cep);
     COMPARE_ZERO_F(cep[1]);
     COMPARE_ZERO_F(cep[2]);
     COMPARE_ZERO_F(cep[3]);
@@ -451,14 +523,15 @@ BOOST_AUTO_TEST_CASE(cepstrumF)
     BOOST_CHECK_SMALL( 7.254329 - cep[4]/8, 0.000001);
 }
 
-BOOST_AUTO_TEST_CASE(forwardArrayBoundsF)
+ALL_IMPL_AUTO_TEST_CASE(forwardArrayBoundsF)
 {
     // initialise bins to something recognisable, so we can tell
     // if they haven't been written
     float in[] = { 1, 1, -1, -1 };
     float re[] = { 999, 999, 999, 999, 999 };
     float im[] = { 999, 999, 999, 999, 999 };
-    FFT(4).forward(in, re+1, im+1);
+    USING_FFT(4);
+    fft.forward(in, re+1, im+1);
     // And check we haven't overrun the arrays
     COMPARE_F(re[0], 999.0f);
     COMPARE_F(im[0], 999.0f);
@@ -466,14 +539,15 @@ BOOST_AUTO_TEST_CASE(forwardArrayBoundsF)
     COMPARE_F(im[4], 999.0f);
 }
 
-BOOST_AUTO_TEST_CASE(inverseArrayBoundsF)
+ALL_IMPL_AUTO_TEST_CASE(inverseArrayBoundsF)
 {
     // initialise bins to something recognisable, so we can tell
     // if they haven't been written
     float re[] = { 0, 1, 0 };
     float im[] = { 0, -2, 0 };
     float out[] = { 999, 999, 999, 999, 999, 999 };
-    FFT(4).inverse(re, im, out+1);
+    USING_FFT(4);
+    fft.inverse(re, im, out+1);
     // And check we haven't overrun the arrays
     COMPARE_F(out[0], 999.0f);
     COMPARE_F(out[5], 999.0f);
